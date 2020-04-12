ddd8599844612c1c7a47349038910ede6e66f877
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2020-04-12 12:04:50 -0400

    insert implemented

--- views/carlos_ferraz_functions.php
+++ views/carlos_ferraz_functions.php
@@ -12,18 +12,35 @@
     $connection = new PDO(
       "mysql:host=$DB_HOST;dbname=$DB_NAME",
       $DB_USER,
-      $DB_PASSWORD
+      $DB_PASSWORD,
+      array(PDO::ATTR_PERSISTENT => true)
     );
+    echo "connected";
   } catch (PDOException $e){
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
   }
 
+  // select records
   try{
-    foreach($connection->query('SELECT * FROM FOO') as $row){
+    $statement = $connection->prepare("SELECT * FROM FOO");
+    $statement->execute();
+    foreach($statement as $row){
       print_r($row);
     }
   } catch (PDOException $e){
+    print "Error! : " . $e->getMessage() . "<br/>";
+  }
+
+  // add record
+  try{
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->beginTransaction();
+    $statement = $connection->prepare("INSERT INTO FOO (name) VALUES ('test')");
+    $statement->execute();
+    $connection->commit();
+  } catch (PDOException $e){
+    $connection->rollBack();
     print "Error! : " . $e->getMessage() . "<br/>";
   }
 
