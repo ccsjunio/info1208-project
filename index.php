c74c6f3a38bda7f5ae4fd06d4f11db212783ce31
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2020-04-12 00:18:42 -0400

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
-  echo "test for auto deploy BBBBBBBBBBBB";
+
+define("_ROOTFOLDER",$_SERVER['DOCUMENT_ROOT']);
+define("_URLBASE",$_SERVER['SERVER_NAME']);
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        http_response_code(200);
+        require __DIR__ . '/views/carlos_ferraz_submit.php';
+        break;
+    case '' :
+        http_response_code(200);
+        require __DIR__ . '/views/carlos_ferraz_submit.php';
+        break;
+    case '/functions' :
+        http_response_code(200);
+        require __DIR__ . '/views/carlos_ferraz_functions.php';
+        break;
+    case '/output' :
+        http_response_code(200);
+        require __DIR__ . '/views/carlos_ferraz_output.php';
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
+
 ?>
